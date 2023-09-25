5e8c104651d2dca8312b698f16d1530931de1d4f
melfx <130380863+melfx@users.noreply.github.com>
2023-09-25 08:54:36 -0300

    Add files via upload
    fhghjhkuky

---
+++ duda/calculo.js
@@ -0,0 +1,13 @@
+//-------------------------------------------
+//daqui pra baixo começa o js do site
+
+let titulo = document.querySelector(".titulo");
+console.log(titulo.textContent);
+//altera o conteudo da variável titulo para o texto desejado
+titulo.textContent = "Calculadora de Notaaaaa";
+
+
+//mudanças na tabela
+let tdNota = nota.querySelector(".info-primeirotrimestre");
+let tdNota = nota.querySelector(".info-segundotrimestre");
+
